91c45b1afa964e24e469c706a8e77620bd7ddd35
iris <1340531394@qq.com>
2020-08-09 23:29:13 +0800

    vuex

--- src/views/Forms/StepForm/Step1.vue
+++ src/views/Forms/StepForm/Step1.vue
@@ -1,15 +1,40 @@
 <template>
-    <div>
-        第一步
-    </div>
+    <a-form :form="form" :label-col="{ span: 5 }" :wrapper-col="{ span: 12 }">
+        <a-form-item label="付款账号">
+            <a-input
+                    v-decorator="['payAccount', {initialValue:step.payCount , rules: [{ required: true, message: '请输入付款账号' }] }]"
+            />
+        </a-form-item>
+        <a-form-item :wrapper-col="{ span: 12, offset: 5 }">
+            <a-button type="primary" @click="handleSubmit">
+                下一步
+            </a-button>
+        </a-form-item>
+    </a-form>
 </template>
 
 <script>
   export default {
-    name: "Step1.vue"
+    data() {
+      return {
+        formLayout: 'horizontal',
+        form: this.$form.createForm(this, { name: 'coordinated' }),
+      };
+    },
+    computed:{
+      step(){
+        return this.$store.state.form.step;
+      }
+    },
+    methods: {
+      handleSubmit(e) {
+        e.preventDefault();
+        this.form.validateFields((err, values) => {
+          if (!err) {
+            console.log('Received values of form: ', values);
+          }
+        });
+      },
+    },
   };
 </script>
-
-<style scoped>
-
-</style>
